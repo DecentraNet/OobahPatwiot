c49100bd6b0a67758343103497def73ee3baf1e6
DecentraNet <89030441+DecentraNet@users.noreply.github.com>
2021-11-12 19:51:44 -0500

    Add files via upload

---
+++ win10.bat
@@ -0,0 +1,385 @@
+@echo off
+setlocal EnableDelayedExpansion
+net session
+
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+  set "DEL=%%a"
+)
+
+rem Prepare a file "X" with only one dot
+<nul > X set /p ".=."
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+call :color 0b "Make sure FORENSICS QUESTIONS are done"
+echo.
+call :color 4e "Make sure FORENSICS QUESTIONS are done"
+echo.
+call :color 19 "Make sure FORENSICS QUESTIONS are done"
+echo.
+echo.
+echo.
+echo.
+call :color 0a "Make sure FORENSICS QUESTIONS are done"
+echo.
+call :color 4e "Make sure FORENSICS QUESTIONS are done"
+echo.
+call :color 0C "Make sure FORENSICS QUESTIONS are done"
+echo.
+echo.
+echo.
+echo.
+call :color 2F "Make sure FORENSICS QUESTIONS are done"
+echo.
+call :color 4e "Make sure FORENSICS QUESTIONS are done"
+echo.
+call :color 0C "Make sure FORENSICS QUESTIONS are done"
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+
+TIMEOUT /T 10
+
+goto :menu
+
+:color
+set "param=^%~2" !
+set "param=!param:"=\"!"
+findstr /p /A:%1 "." "!param!\..\X" nul
+<nul set /p ".=%DEL%%DEL%%DEL%%DEL%%DEL%%DEL%%DEL%"
+exit /b
+
+:menu
+	cls
+	echo 
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	call :color 0b "	      WELCOME to THY MEAGER WIN10 SCRIPT, created by		"	 
+	echo.
+	echo "		___________                       ________		   "
+	echo "		\__    ___/___ _____    _____    /  _____/		   "
+	echo "		  |    |_/ __ \\__  \  /     \  /   __  \ 		   "
+	echo "		  |    |\  ___/ / __ \|  Y Y  \ \  |__\  \		   "
+	echo "		  |____| \___  >____  /__|_|  /  \_____  /		   "
+	echo "		             \/     \/      \/         \/ 		   "
+	echo "		DAVID | IAN | JAMES | VARUN | OWEN | EDWARD		   "
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	echo "1) Set user properties			2) Change all passwords		   "
+	echo "3) Disable guests/admins		4) Change password policy	   "
+	echo "5) Set lockout policies		6) Enable Firewall		   "
+	echo "7) Bad files (TESTING)			8) Disable services		   "
+	echo "9) Remote desktop configurations	10) Enable auto updates		   "
+	echo "11) Security options			12) Audit machine		   "
+	echo "13) Group editing	(reg)		14)Security options		   "
+	echo "15)Audit the machine			16)Edit groups (BETA)		   "
+	echo "E) Exit				R) Reboot			   "
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	set /p answer=Please choose an option for execution of Order 66: 
+		if "%answer%"=="1" goto :userProp
+		if "%answer%"=="2" goto :passwd
+		if "%answer%"=="3" goto :disGueAdm
+		if "%answer%"=="4" goto :passwdPol
+		if "%answer%"=="5" goto :lockout
+		if "%answer%"=="6" goto :firewall
+		if "%answer%"=="7" goto :badFiles
+		if "%answer%"=="8" goto :services
+		if "%answer%"=="9" goto :remDesk
+		if "%answer%"=="10" goto :autoUpdate
+		if "%answer%"=="11" goto :secOpt
+		if "%answer%"=="12" goto :audit
+		if "%answer%"=="13" goto :group
+		if "%answer%"=="E" exit
+		if "%answer%"=="R" shutdown /rs
+	pause
+
+:userProp
+	echo Setting password never expires
+	wmic UserAccount set PasswordExpires=True
+	wmic UserAccount set PasswordChangeable=True
+	wmic UserAccount set PasswordRequired=True
+	goto :menu
+
+:passwd
+	endlocal
+	setlocal EnableExtensions
+	for /F "tokens=2* delims==" %%G in ('
+		wmic UserAccount where "status='ok'" get name >null
+	') do for %%g in (%%~G) do (
+		net user %%~g Cyberpatriot1!
+		)
+	endlocal
+	setlocal enabledelayedexpansion	
+	pause
+	goto :menu
+	
+:disGueAdm
+	net user Guest | findstr Active | findstr Yes
+	if %errorlevel%==0 (
+		echo Guest account is already disabled.
+	)
+	if %errorlevel%==1 (
+		net user guest Cyberpatriot1! /active:no
+	)
+	
+	rem Disables the Admin account
+	net user Administrator | findstr Active | findstr Yes
+	if %errorlevel%==0 (
+		echo Admin account is already disabled.
+
+		goto :menu
+	)
+	if %errorlevel%==1 (
+		net user administrator Cyberpatriot1! /active:no
+
+		goto :menu
+	)
+	
+:passwdPol
+	rem Sets the password policy
+	rem Set complexity requirments
+	echo Setting pasword policies
+	net accounts /minpwlen:10
+	net accounts /maxpwage:30
+	net accounts /minpwage:5
+	net accounts /uniquepw:3
+	goto :menu
+	
+:lockout
+	rem Sets the lockout policy
+	echo Setting the lockout policy
+	net accounts /lockoutduration:20
+	net accounts /lockoutthreshold:5
+	net accounts /lockoutwindow:20
+	goto :menu
+	
+:firewall
+	rem Enables firewall
+	netsh advfirewall set allprofiles state on
+	netsh advfirewall reset
+	goto :menu
+	
+:badFiles
+	goto :menu
+
+:services
+	echo Disabling Services
+	sc stop TapiSrv
+	sc config TapiSrv start= disabled
+	sc stop TlntSvr
+	sc config TlntSvr start= disabled
+	sc stop ftpsvc
+	sc config ftpsvc start= disabled
+	sc stop SNMP
+	sc config SNMP start= disabled
+	sc stop SessionEnv
+	sc config SessionEnv start= disabled
+	sc stop TermService
+	sc config TermService start= disabled
+	sc stop UmRdpService
+	sc config UmRdpService start= disabled
+	sc stop SharedAccess
+	sc config SharedAccess start= disabled
+	sc stop remoteRegistry 
+	sc config remoteRegistry start= disabled
+	sc stop SSDPSRV
+	sc config SSDPSRV start= disabled
+	sc stop W3SVC
+	sc config W3SVC start= disabled
+	sc stop SNMPTRAP
+	sc config SNMPTRAP start= disabled
+	sc stop remoteAccess
+	sc config remoteAccess start= disabled
+	sc stop RpcSs
+	sc config RpcSs start= disabled
+	sc stop HomeGroupProvider
+	sc config HomeGroupProvider start= disabled
+	sc stop HomeGroupListener
+	sc config HomeGroupListener start= disabled
+	goto :menu
+
+:remDesk
+	rem Ask for remote desktop
+	set /p answer=Remove remote desktop? [Y / N]
+	if /I "%answer%"=="N" (
+		reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 1 /f
+		echo RemoteDesktop has been disabled, reboot for this to take full effect.
+	)
+	if /I "%answer%"=="Y" (
+		reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+		echo RemoteDesktop has been enabled, reboot for this to take full effect.
+	)
+	goto :menu
+	
+:autoUpdate
+	echo AutoUpdate executing...
+	reg add "HKLM\SOFTWARE\Microsoft\WINDOWS\CurrentVersion\WindowsUpdate\Auto Update" /v AUOptions /t REG_DWORD /d 4 /f
+	echo AutoUpdate executed!
+	goto :menu
+	
+:secOpt
+	echo Changing security options now.
+
+	rem Restrict CD ROM drive
+	reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AllocateCDRoms /t REG_DWORD /d 1 /f
+
+	rem Automatic Admin logon
+	reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_DWORD /d 0 /f
+	
+	rem Logon message text
+	set /p body=Please enter logon text: 
+		reg ADD "HKLM\SYSTEM\microsoft\Windwos\CurrentVersion\Policies\System\legalnoticetext" /v LegalNoticeText /t REG_SZ /d "%body%"
+	
+	rem Logon message title bar
+	set /p subject=Please enter the title of the message: 
+		reg ADD "HKLM\SYSTEM\microsoft\Windwos\CurrentVersion\Policies\System\legalnoticecaption" /v LegalNoticeCaption /t REG_SZ /d "%subject%"
+	
+	rem Wipe page file from shutdown
+	reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v ClearPageFileAtShutdown /t REG_DWORD /d 1 /f
+	
+	rem Disallow remote access to floppie disks
+	reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AllocateFloppies /t REG_DWORD /d 1 /f
+	
+	rem Prevent print driver installs 
+	reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Print\Providers\LanMan Print Services\Servers" /v AddPrinterDrivers /t REG_DWORD /d 1 /f
+	
+	rem Limit local account use of blank passwords to console
+	reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v LimitBlankPasswordUse /t REG_DWORD /d 1 /f
+	
+	rem Auditing access of Global System Objects
+	reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v auditbaseobjects /t REG_DWORD /d 1 /f
+	
+	rem Auditing Backup and Restore
+	reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v fullprivilegeauditing /t REG_DWORD /d 1 /f
+	
+	rem Do not display last user on logon
+	reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v dontdisplaylastusername /t REG_DWORD /d 1 /f
+	
+	rem UAC setting (Prompt on Secure Desktop)
+	reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v PromptOnSecureDesktop /t REG_DWORD /d 1 /f
+	
+	rem Enable Installer Detection
+	reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableInstallerDetection /t REG_DWORD /d 1 /f
+	
+	rem Undock without logon
+	reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v undockwithoutlogon /t REG_DWORD /d 0 /f
+	
+	rem Maximum Machine Password Age
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v MaximumPasswordAge /t REG_DWORD /d 15 /f
+	
+	rem Disable machine account password changes
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v DisablePasswordChange /t REG_DWORD /d 1 /f
+	
+	rem Require Strong Session Key
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v RequireStrongKey /t REG_DWORD /d 1 /f
+	
+	rem Require Sign/Seal
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v RequireSignOrSeal /t REG_DWORD /d 1 /f
+	
+	rem Sign Channel
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v SignSecureChannel /t REG_DWORD /d 1 /f
+	
+	rem Seal Channel
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\Netlogon\Parameters /v SealSecureChannel /t REG_DWORD /d 1 /f
+	
+	rem Don't disable CTRL+ALT+DEL even though it serves no purpose
+	reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableCAD /t REG_DWORD /d 0 /f 
+	
+	rem Restrict Anonymous Enumeration #1
+	reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v restrictanonymous /t REG_DWORD /d 1 /f 
+	
+	rem Restrict Anonymous Enumeration #2
+	reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v restrictanonymoussam /t REG_DWORD /d 1 /f 
+	
+	rem Idle Time Limit - 45 mins
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v autodisconnect /t REG_DWORD /d 45 /f 
+	
+	rem Require Security Signature - Disabled pursuant to checklist
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v enablesecuritysignature /t REG_DWORD /d 0 /f 
+	
+	rem Enable Security Signature - Disabled pursuant to checklist
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v requiresecuritysignature /t REG_DWORD /d 0 /f 
+	
+	rem Disable Domain Credential Storage
+	reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v disabledomaincreds /t REG_DWORD /d 1 /f 
+	
+	rem Don't Give Anons Everyone Permissions
+	reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v everyoneincludesanonymous /t REG_DWORD /d 0 /f 
+	
+	rem SMB Passwords unencrypted to third party
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanWorkstation\Parameters /v EnablePlainTextPassword /t REG_DWORD /d 0 /f
+	
+	rem Null Session Pipes Cleared
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v NullSessionPipes /t REG_MULTI_SZ /d "" /f
+	
+	rem remotely accessible registry paths cleared
+	reg ADD HKLM\SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg\AllowedExactPaths /v Machine /t REG_MULTI_SZ /d "" /f
+	
+	rem remotely accessible registry paths and sub-paths cleared
+	reg ADD HKLM\SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg\AllowedPaths /v Machine /t REG_MULTI_SZ /d "" /f
+	
+	rem Restict anonymous access to named pipes and shares
+	reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v NullSessionShares /t REG_MULTI_SZ /d "" /f
+	
+	rem Allow to use Machine ID for NTLM
+	reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v UseMachineId /t REG_DWORD /d 0 /f
+
+	rem Clear pagefile on shutdown (BETA)
+	reg ADD HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management /v ClearPageFileAtShutdown /t REG_DWORD /d 1 /f
+
+	rem Smartscreen enabled (BETA)
+	reg ADD HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System /v EnableSmartScreen /t REG_DWORD /d 1 /f
+
+	rem Net tcp port sharing disabled (BETA)
+	reg ADD HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services\NetTcpPortSharing /v Start /t REG_DWORD /d 4 /f
+
+	rem Enable UAC (BETA)
+	reg ADD HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLua /t REG_DWORD /d 1 /f
+	rem Enables DEP
+	bcdedit.exe /set {current} nx AlwaysOn
+	goto :menu
+	
+:audit
+	echo Auditing machine...
+	auditpol /set /category:* /success:enable
+	auditpol /set /category:* /failure:enable
+	echo Auditing enabled!
+	goto :menu
+
+:group
+	cls
+	net localgroup
+	set /p grp=What group would you like to check?:
+	net localgroup !grp!
+	set /p answer=Is there a user you would like to add or remove?[add/remove/back]:
+	if "%answer%"=="add" (
+		set /p userAdd=Please enter the user you would like to add: 
+		net localgroup !grp! !userAdd! /add
+		echo !userAdd! has been added to !grp!
+	)
+	if "%answer%"=="remove" (
+		set /p userRem=Please enter the user you would like to remove:
+		net localgroup !grp! !userRem! /delete
+		echo !userRem! has been removed from !grp!
+	)
+	if "%answer%"=="back" (
+		goto :group
+	)
+
+	set /p answer=Would you like to go check again?[y/n]
+	if /I "%answer%"=="y" (
+		goto :group
+	)
+	if /I "%answer%"=="n" (
+		goto :menu
+	)
+endlocal
